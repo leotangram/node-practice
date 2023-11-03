06dad90b96a0c7b939d7ad65f31b12484bf8ff6b
Leonardo Omaña <leotangram@gmail.com>
2023-11-02 20:08:28 -0500

    arrow functions

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,15 +1,13 @@
 // const { emailTemplate } = require('./js-foundation/01-template')
 // require('./js-foundation/02-destructuring')
-require('./js-foundation/03-callbacks')
+require('./js-foundation/04-arrow')
 // import { getUserById } from './js-foundation/03-callbacks'
 const { getUserById } = require('./js-foundation/03-callbacks')
 
-const id = 1
+const id = 2
 
-getUserById(id, function (error, user) {
-  if (error) {
-    throw new Error(error)
-  }
+getUserById(id, (error, user) => {
+  if (error) throw new Error(error)
 
   console.log({ user })
 })
---
+++ 02-bases/src/js-foundation/04-arrow.js
@@ -0,0 +1,32 @@
+const users = [
+  {
+    id: 1,
+    name: 'John Doe'
+  },
+  {
+    id: 2,
+    name: 'Jane Doe'
+  }
+]
+
+// export function getUserById(id) {
+//   const user = users.find(function (user) {
+//     return user.id === id
+//   })
+
+//   console.log({ user })
+// }
+
+const getUserById = (id, callback) => {
+  const user = users.find(user => user.id === id)
+
+  if (!user) {
+    return callback(`User not found with id ${id}`)
+  }
+
+  return callback(null, user)
+}
+
+module.exports = {
+  getUserById
+}
