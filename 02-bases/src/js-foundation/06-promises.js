9eb579498e34135837b2c0def78f8ab19507060e
Leonardo Omaña <leotangram@gmail.com>
2023-11-05 17:36:22 -0500

    async await

--- 02-bases/src/js-foundation/06-promises.js
+++ 02-bases/src/js-foundation/06-promises.js
@@ -1,8 +1,13 @@
-const getPokemonById = id => {
+const getPokemonById = async id => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`
-  return fetch(url)
-    .then(response => response.json())
-    .then(pokemon => pokemon.name)
+  const response = await fetch(url)
+  const pokemon = await response.json()
+
+  return pokemon.name
+  //   .then(response => response.json())
+  //   .then(pokemon => pokemon.name)
+
+  return 1
 }
 
 module.exports = getPokemonById
