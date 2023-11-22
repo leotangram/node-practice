41514e9e7e1dba6415ed06413cc89e34c6b1129a
Leonardo Omaña <leotangram@gmail.com>
2023-11-22 15:53:36 -0500

    arrange, act & assert

--- 02-node-ts-typescript/src/tests/app.tests.ts
+++ 02-node-ts-typescript/src/tests/app.tests.ts
@@ -1,5 +1,10 @@
 describe('App', () => {
-  test('should be true', () => {
-    expect(true).toBe(true)
+  test('should be 30', () => {
+    const num1 = 10
+    const num2 = 20
+
+    const result = num1 + num2
+
+    expect(result).toBe(30)
   })
 })
