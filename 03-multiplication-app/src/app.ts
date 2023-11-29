52a712d156b8c4933fbe77b17defc4a31c1412e8
Leonardo Omaña <leotangram@gmail.com>
2023-11-28 21:41:52 -0500

    print in cosole & file

--- 03-multiplication-app/src/app.ts
+++ 03-multiplication-app/src/app.ts
@@ -1,3 +1,29 @@
-const message = 'Hello World'
+const { mkdirSync, writeFileSync } = require('fs')
 
-console.log(message)
+let outputMessage = ''
+const base = 5
+const headerMessage = `
+==================
+   Table ${base}
+==================\n
+`
+
+for (let i = 1; i <= 10; i++) {
+  outputMessage += `${base} x ${i} = ${base * i}\n`
+}
+
+outputMessage = headerMessage + outputMessage
+
+console.log(outputMessage)
+
+const outputPath = `outputs`
+
+mkdirSync(outputPath, { recursive: true })
+
+writeFileSync(
+  `${outputPath}/table-${base}.txt`,
+  outputMessage,
+  (error: any) => {
+    if (error) throw error
+  }
+)
